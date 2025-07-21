10fb40b56f904cbf97018baf43fdcd88ecbd355c
koyyo <dkamen74@gmail.com>
2025-07-21 10:04:26 -0600

    semresults

--- lime/tests/test_lime_parser.py
+++ lime/tests/test_lime_parser.py
@@ -541,7 +541,7 @@ descs = {
 comp_descs = {
     "models": [model_save_name(p, DATASET, ext=False) for p in model_params],
     "parses": ["Dep", "Con", "Ran"],
-    "disting": "IMDBResults1"
+    "disting": "SemResults1"
 }
 
 instance_idxs = list(range(25))
