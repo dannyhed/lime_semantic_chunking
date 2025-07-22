273963e400c0433e8d656eaaf46af26b8c44763d
koyyo <dkamen74@gmail.com>
2025-07-22 17:14:24 -0600

    whyzeros2

--- lime/tests/test_lime_parser.py
+++ lime/tests/test_lime_parser.py
@@ -315,6 +315,8 @@ def influence_sz(exp, label=1):
 
     if not exp.data["is_standard"]:
         local_exp = exp.get_local_exp()
+        if len(local_exp):
+            print(exp.get_tokens())
         ids = [x[0] for x in local_exp]
 
         dependence_chunks = [[] for _ in range(int(max(ids)/num_words) + 1)]
@@ -331,8 +333,6 @@ def influence_sz(exp, label=1):
         relation_distance = [abs(max(id) - min(id)) for id in id_chunks]
         weighted_rel_dist = [dist * abs(weighted_dep_chunks[i]) for i, dist in enumerate(relation_distance)]
         total = len(id_chunks)
-        if total == 0:
-            print(exp.get_tokens())
 
     else:
         get_exp = exp.get_exp().local_exp[label]
