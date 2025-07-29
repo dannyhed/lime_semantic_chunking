da463ac467b5cb26534df4e7cd48830537021be5
koyyo <dkamen74@gmail.com>
2025-07-29 17:23:32 -0600

    predict_proba

--- lime/tests/test_lime_parser.py
+++ lime/tests/test_lime_parser.py
@@ -193,7 +193,7 @@ def run_all_explainers(models, class_names, parameter_sets, instances, save=Fals
                         # background = shap_train[:100]
                         # mask = shap.maskers.Text(r"\W+")
                         # sh = shap.KernelExplainer(model.predict_proba, background)
-                        sh = shap.Explainer(model)
+                        sh = shap.Explainer(model.predict_proba)
                         explanations.append(sh(inst))
                         new_expls += 1
                     elif just_desc:
