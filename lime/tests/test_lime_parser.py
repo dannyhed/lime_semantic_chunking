49a15e27a16b11c35f5171e31555c828b31beee0
koyyo <dkamen74@gmail.com>
2025-12-18 03:04:33 -0700

    retry 3d stab

--- lime/tests/test_lime_parser.py
+++ lime/tests/test_lime_parser.py
@@ -1212,7 +1212,7 @@ def obj_metrics(num_sens=50, num_syns=10):
                   "dep": LimeTextParserExplainer(verbose=False, parsing_type="dependency"),
                   "con" : LimeTextParserExplainer(verbose=False, parsing_type="constituency")}
     
-    stab = [[[stability(explainers, m) for m in pset] for pset in dset] for dset in ALL_DATASETS]
+    stab = [[[stability(explainers, m) for m in pset] for pset in dset] for dset in all_models]
     print(stab)
     
 
